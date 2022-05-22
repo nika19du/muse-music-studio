989bdc35c2340775372c132abde716bd1a0fcfae
nika19du <qnkovanikol29@gmail.com>
2022-05-22 22:00:02 +0300

    fixing navbar and added icon in tab

--- script.js
+++ script.js
@@ -42,3 +42,36 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
     e.target.elements.email.value = '';
     e.target.elements.message.value = '';
   });
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.animate-hero',{
+    duration:0.6,
+    opacity:0,
+    y:-150,
+    stagger:0.6
+});
+gsap.from('.animate-box',{
+    scrollTrigger:'.animate-box',
+    duration:0.5,
+    opacity:2, 
+    x:-150,
+    stagger:0.12
+});
+gsap.from('.animate-card',{
+    scrollTrigger:'.animate-card',
+    duration:0.8,
+    opacity:2, 
+    y:50
+    // duration:1,
+    // opacity:0,
+    // y: 50,
+    // stagger:0.1,
+    // delay:0.2
+});
+gsap.from('.animate-top-box',{
+    scrollTrigger:'.animate-top-box',
+    duration:0.8,
+    opacity:2, 
+    y:50
+});
